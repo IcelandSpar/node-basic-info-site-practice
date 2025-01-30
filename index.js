c406251005bd367b66b1dffbd3482100f1c348a8
IcySpar <victorasielrodriguez@gmail.com>
2025-01-29 23:51:21 -0800

    feat: add basic routing for about and index pages

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
 
 http.createServer((req, res) => {
+
+    let pathName = req.url;
+    let fileName = () => {
+        if(req.url == '/') {
+            return './index.html';
+        } else if(pathName == '/about') {
+            return './about.html';
+        }
+    }
+
+
+    fs.readFile(fileName(), 'utf8', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    })
+
+
+
 
 }).listen(8080);
